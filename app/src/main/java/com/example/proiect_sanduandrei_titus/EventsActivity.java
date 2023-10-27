59d8dbb131c6498c5c267304e5e23bd8c45263e0
andreititussandu <andrei.titus.sandu@gmail.com>
2023-10-27 15:09:33 +0300

    added ratings form

--- app/src/main/java/com/example/proiect_sanduandrei_titus/EventsActivity.java
+++ app/src/main/java/com/example/proiect_sanduandrei_titus/EventsActivity.java
@@ -11,4 +11,5 @@ public class EventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
     }
+    //aici sa adaug un datepicker pt evenimente
 }
